c661b077d7629d9cd27e6e3c90ba0ebcf9b2b59d
LucidMinded <ilyaa89nur@gmail.com>
2024-02-26 00:00:14 +0300

    hw2 task3 fixed

--- scrapper/src/main/java/edu/java/LinkUpdateScheduler.java
+++ scrapper/src/main/java/edu/java/LinkUpdateScheduler.java
@@ -2,6 +2,7 @@ package edu.java;
 
 import edu.java.client.github.GithubClient;
 import edu.java.client.stackoverflow.StackoverflowClient;
+import edu.java.configuration.ApplicationConfig;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -10,9 +11,10 @@ import org.springframework.stereotype.Component;
 @Slf4j @AllArgsConstructor @Component public class LinkUpdateScheduler {
     private final GithubClient githubClient;
     private final StackoverflowClient stackoverflowClient;
+    private final ApplicationConfig applicationConfig;
 
-    @Scheduled(fixedDelay = 60 * 1000)
-//    @Scheduled(fixedDelayString = "${app.scheduler.interval}")
+    //    @Scheduled(fixedDelay = 60 * 1000)
+    @Scheduled(fixedDelayString = "#{@scheduler.interval().toMillis()}")
     public void update() {
         log.info("Updating links...");
     }
